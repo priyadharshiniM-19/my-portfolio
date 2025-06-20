f23ea25df193402356d5846d89f9fd711fecf8d4
priayadharshiniM-19 <priyadharshinim090805@gmail.com>
2025-06-20 19:52:04 +0530

    Update vite.config.js for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/my-portfolio/',  // ✅ CHANGE THIS to match your GitHub repo name
+    base: '/my-portfolio/',  
     plugins: [react()],
     build: {
         rollupOptions: {
